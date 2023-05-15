6ab27cf7ff053e962ae93cd8969eb8446a6126dd
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2023-05-15 18:51:24 +0300

    setup basic configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      load: [configuration],
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => {
-        const environment = configService.get<string>('NODE_ENV');
-        console.log(environment);
+        const environment = configService.get<string>('environment');
+        const databaseHost = configService.get<string>('database.host');
+        const databasePort = configService.get<number>('database.port');
+        const databaseUsername = configService.get<string>('database.username');
+        const databasePassword = configService.get<string>('database.password');
+        const databaseName = configService.get<string>('database.name');
+
         return {
           type: 'mysql',
-          host: 'mysql',
-          port: 3306,
-          username: 'root',
-          password: 'root',
-          database: 'library',
+          host: databaseHost,
+          port: databasePort,
+          username: databaseUsername,
+          password: databasePassword,
+          database: databaseName,
           entities: [],
           synchronize: environment === 'development' ? true : false,
         };
---
+++ src/config/configuration.ts
@@ -0,0 +1,11 @@
+export default () => ({
+  environment: process.env.NODE_ENV,
+  port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+  },
+});
