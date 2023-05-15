dcfacb0292d54e5728e4867e7fc836000de5f8fb
Krutilin Sergey <krutilin.sergey.ks@gmail.com>
2023-05-15 18:34:28 +0300

    setup docker compose up (nestjs, mysql)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const environment = configService.get<string>('NODE_ENV');
+        console.log(environment);
+        return {
+          type: 'mysql',
+          host: 'mysql',
+          port: 3306,
+          username: 'root',
+          password: 'root',
+          database: 'library',
+          entities: [],
+          synchronize: environment === 'development' ? true : false,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
